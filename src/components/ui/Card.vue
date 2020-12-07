be3f16e6b5ea3a0cf81cc73bdb4536081b755254
evillt <ijoec123@gmail.com>
2020-12-08 01:40:53 +0800

    feat(ui): card

---
+++ src/components/ui/Card.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="card"><slot /></div>
+</template>
+
+<style lang="less" scoped>
+.card {
+  border-radius: 10px;
+  overflow: hidden;
+
+  & + .card {
+    margin-top: var(--inline-gap);
+  }
+}
+</style>
