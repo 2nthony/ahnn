821b6ddd083e867853818846da8437e2c397dfa2
evillt <ijoec123@gmail.com>
2020-12-07 17:26:39 +0800

    fix(page-setting): pkg version

--- src/views/me/setting.vue
+++ src/views/me/setting.vue
@@ -10,7 +10,7 @@
       <CellItem
         title="关于 Ahnn"
         :link="'/me/about-app'"
-        :rightText="'版本 0.0.1'"
+        :rightText="`版本 ${version}`"
       ></CellItem>
     </Cell>
 
@@ -19,6 +19,7 @@
 </template>
 
 <script>
+/* global __APP_VERSION__ */
 import Cell from '../../components/Cell.vue'
 import CellItem from '../../components/CellItem.vue'
 import Tabbar from '../../components/Tabbar.vue'
@@ -26,5 +27,11 @@ import ViewingArea from '../../layout/ViewingArea.vue'
 
 export default {
   components: { Tabbar, ViewingArea, Cell, CellItem },
+
+  setup() {
+    return {
+      version: __APP_VERSION__
+    }
+  }
 }
 </script>
