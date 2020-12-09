dabe627351513bad8b0a0fab6be09e5d6180ddf6
evillt <ijoec123@gmail.com>
2020-12-09 21:30:46 +0800

    feat: global styles

--- src/App.vue
+++ src/App.vue
@@ -13,46 +13,4 @@ export default defineComponent({
 <style lang="less" src="./styles/var.less"></style>
 <style lang="less" src="./styles/weird.less"></style>
 
-<style lang="less">
-html,
-body {
-  padding: 0;
-  margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-  width: 100%;
-  height: 100%;
-}
-
-body {
-  background-color: var(--bg-color);
-}
-
-a {
-  color: inherit;
-  text-decoration: none;
-}
-
-* {
-  box-sizing: border-box;
-}
-
-h1,
-h2,
-h3,
-h4,
-h5,
-h6 {
-  margin: 0;
-}
-
-#app {
-  color: var(--text-color-default);
-  padding-bottom: calc(
-    var(--tabbar-height) + var(--gap) + env(safe-area-inset-bottom)
-  );
-
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-</style>
+<style lang="less" src="./styles/global.less"></style>
