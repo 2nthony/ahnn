da8746a9e85ff823c030123912bc177baf499762
evillt <ijoec123@gmail.com>
2020-12-09 14:20:22 +0800

    feat(model-type): cn text

--- src/model/Type.ts
+++ src/model/Type.ts
@@ -3,6 +3,11 @@ export enum Types {
   income = 'income',
 }
 
+export const TypeCNTexts = {
+  [Types.payout]: '支出',
+  [Types.income]: '收入',
+}
+
 export type Type = Types.income | Types.payout
 
 export const defaultType: Type = Types.payout
