482f71d7be134b00ccab15daee4c3371a4c94fcc
evillt <ijoec123@gmail.com>
2020-12-10 12:02:36 +0800

    feat(utils): falsy truthy

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,8 @@
 export const isBrowser = typeof window !== 'undefined'
 
 export const deepToRaw = (val: any) => JSON.parse(JSON.stringify(val))
+
+export const isFalsy = (val: any) => {
+  return [null, undefined, NaN].indexOf(val) !== -1
+}
+export const isTruthy = (val: any) => !isFalsy(val)
