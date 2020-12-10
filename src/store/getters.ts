659a6d5ac39aca622d872c5732a42eeac2c44eee
evillt <ijoec123@gmail.com>
2020-12-11 02:08:36 +0800

    feat: Preference

--- src/store/getters.ts
+++ src/store/getters.ts
@@ -1,11 +1,14 @@
 import { Record } from '../model/Record'
 import { GetterTree } from 'vuex'
 import { State } from './state'
+import { Preference } from '@/model/Preference'
 
 export type Getters = {
   addRecord(state: State): Record
+  preference(state: State): Preference
 }
 
 export const getters: GetterTree<State, State> & Getters = {
   addRecord: (state) => state.addRecord,
+  preference: (state) => state.preference,
 }
--- src/store/state.ts
+++ src/store/state.ts
@@ -1,10 +1,13 @@
+import { getInitPreference, Preference } from '@/model/Preference'
 import { getInitRecord, Record } from '../model/Record'
 
 export interface State {
   records: Record[]
   addRecord: Record
+  preference: Preference
 }
 export const state: State = {
   records: [],
   addRecord: getInitRecord(),
+  preference: getInitPreference(),
 }
