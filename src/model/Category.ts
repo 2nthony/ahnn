5097b0d4b960858619a4d1f050e078a4463f9b25
evillt <ijoec123@gmail.com>
2020-12-10 16:06:28 +0800

    feat(model-category): add bulk

--- src/model/Category.ts
+++ src/model/Category.ts
@@ -20,14 +20,21 @@ export const presetCategories: Category = {
     c('餐饮', 'cake-3'),
     c('娱乐', 'gamepad'),
     c('购物', 'shopping-bag-3'),
-    c('住房', 'home-smile'),
     c('交通', 'subway'),
+    c('住房', 'home-smile'),
     c('话费', 'smartphone'),
+    c('礼物', 'gift'),
     c('旅游', 'road-map'),
     c('医药', 'capsule'),
     c('健康', 'heart-3'),
+    c('投资', 'funds'),
   ],
-  income: [c('工资', 'money-cny-circle'), c('红包', 'red-packet')],
+  income: [
+    c('工资', 'money-cny-circle'),
+    c('红包', 'red-packet'),
+    c('奖金', 'coin'),
+    c('投资', 'funds'),
+  ],
 }
 
 export function getDefaultCategory(type: Type): CategoryItem {
