93f3785d8494934fc4cd8f72492aa18e2e3e6470
evillt <ijoec123@gmail.com>
2020-12-11 03:07:38 +0800

    feat(db): preference

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,13 +1,15 @@
 import { IDBPDatabase, IDBPObjectStore, IDBPTransaction, openDB } from 'idb'
+import { upgradePreference } from './preference'
 import { upgradeRecordDB } from './record'
 
 const DB_NAME = 'Ahnn'
-export const DB_VERSION = 2
+export const DB_VERSION = 1
 
 export async function open() {
   return await openDB(DB_NAME, DB_VERSION, {
     upgrade(db, _oldVersion, _newVersion, transaction) {
       upgradeRecordDB(db, transaction)
+      upgradePreference(db, transaction)
     },
   })
 }
---
+++ src/db/preference.ts
@@ -0,0 +1,31 @@
+import { Preference } from '@/model/Preference'
+import { IDBPDatabase, IDBPTransaction } from 'idb'
+import { ensureStore, open } from '.'
+
+export function upgradePreference(
+  db: IDBPDatabase,
+  transaction: IDBPTransaction,
+) {
+  ensureStore(db, transaction, 'preference', {
+    autoIncrement: true,
+    keyPath: 'id',
+  })
+}
+
+export async function readPreference() {
+  const db = await open()
+  return db
+    .getAll('preference', undefined, 1)
+    .then((res) => res[0])
+    .finally(() => db.close())
+}
+
+export async function setPreference(preference: Preference) {
+  const currentPreference = await readPreference()
+  const action = currentPreference ? 'put' : 'add'
+  const db = await open()
+  return db[action]('preference', {
+    ...currentPreference,
+    ...preference,
+  }).finally(() => db.close())
+}
