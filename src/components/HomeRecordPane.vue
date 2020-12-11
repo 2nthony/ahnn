fa51702bd1473df097cfcd475ae610fcb7aa8d4d
evillt <ijoec123@gmail.com>
2020-12-11 19:40:19 +0800

    feat(home-record-pane): more detail

--- src/components/HomeRecordPane.vue
+++ src/components/HomeRecordPane.vue
@@ -1,18 +1,33 @@
 <template>
-  <Pane class="home-record-pane" :float="3">
+  <Pane class="home-record-pane" :float="3" @click="handleShowMore">
     <div class="top">
       <div class="title">
         <RemixIcon :icon="record.category.icon" />
         <Text>{{ record.category.name }}</Text>
       </div>
       <div class="cost">
-        <Text>{{ Number(record.cost).toFixed(2) }}</Text>
+        <RemixIcon :icon="typeIcon[record.type]" />
+        <Text> {{ Number(record.cost).toFixed(2) }}</Text>
       </div>
     </div>
 
     <div class="bottom" v-if="record.remark">
-      <Text class="remark" :color="'muted'">{{ record.remark }}</Text>
+      <Text class="remark" :class="{ ellpsis: !showMore }">{{
+        record.remark
+      }}</Text>
     </div>
+
+    <template v-if="showMore">
+      <div class="more-detail">
+        <Text>{{ record.date }}</Text>
+        <Text>{{ record.wallet }}</Text>
+      </div>
+
+      <div class="action-group">
+        <Button @click.stop="handleEdit">修改</Button>
+        <Button type="error" @click.stop="handleDelete">删除</Button>
+      </div>
+    </template>
   </Pane>
 </template>
 
@@ -22,19 +37,69 @@ import Pane from './ui/Pane.vue'
 import RemixIcon from './RemixIcon.vue'
 import Text from './ui/Text.vue'
 import Heading from './ui/Heading.vue'
-import { TypeCNTexts } from '@/model/Type'
+import { TypeCNTexts, Types } from '@/model/Type'
+import { defineComponent, ref, watch } from 'vue'
+import Button from './ui/Button.vue'
+import { useRouter } from 'vue-router'
+import { useStore } from '@/store'
+import { MutationTypes } from '@/store/mutations'
+import { Record } from '@/model/Record'
+import { deleteRecord } from '@/db'
+import { createToast, destoryAllToasts } from 'vercel-toast'
+import 'vercel-toast/dist/vercel-toast.css'
 
-export default {
-  components: { Pane, RemixIcon, Text, Heading },
+export default defineComponent({
+  components: { Pane, RemixIcon, Text, Heading, Button },
 
   props: {
     record: setProps('object'),
   },
 
-  setup() {
-    return { TypeCNTexts }
+  setup(props) {
+    const router = useRouter()
+    const store = useStore()
+
+    const showMore = ref(false)
+    const typeIcon = {
+      [Types.income]: 'add',
+      [Types.payout]: 'subtract',
+    }
+
+    watch(showMore, destoryAllToasts)
+
+    const handleShowMore = () => {
+      showMore.value = !showMore.value
+    }
+
+    const handleEdit = () => {
+      store.commit(MutationTypes.setAddRecord, props.record as Record)
+      router.push('/add-record')
+    }
+
+    // TODO 刷新主页数据
+    const handleDelete = () => {
+      createToast('确认删除这条记录吗？', {
+        type: 'error',
+        cancel: '关闭',
+        action: {
+          text: '删除',
+          callback(toast) {
+            deleteRecord(props.record as Record).then(toast.destory)
+          },
+        },
+      })
+    }
+
+    return {
+      TypeCNTexts,
+      showMore,
+      typeIcon,
+      handleShowMore,
+      handleEdit,
+      handleDelete,
+    }
   },
-}
+})
 </script>
 
 <style lang="less" scoped>
@@ -51,7 +116,6 @@ export default {
     display: flex;
     justify-content: space-between;
   }
-
   & .title {
     display: flex;
     align-items: center;
@@ -62,16 +126,36 @@ export default {
       margin-right: var(--inline-gap);
     }
   }
+  & .cost {
+    display: flex;
+    align-items: center;
+  }
 
   & .bottom {
     margin-top: var(--gap);
 
     & .remark {
-      overflow: hidden;
-      text-overflow: ellipsis;
-      display: block;
-      white-space: nowrap;
+      // color: var(--ahnn-secondary);
+
+      &.ellpsis {
+        display: block;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
     }
+  }
+
+  & .more-detail {
+    margin-top: var(--gap);
+    display: flex;
+    justify-content: space-between;
+  }
+
+  & .action-group {
+    margin-top: var(--gap);
+    display: flex;
+    justify-content: flex-end;
   }
 }
 </style>
