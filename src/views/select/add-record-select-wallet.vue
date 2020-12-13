95bf3e21bf437e64216c2b9c1eeb2824aed86c52
evillt <ijoec123@gmail.com>
2020-12-13 14:31:04 +0800

    fix(add-record-select-wallet): should use user added wallet

--- src/views/select/add-record-select-wallet.vue
+++ src/views/select/add-record-select-wallet.vue
@@ -1,25 +1,35 @@
 <template>
   <PageSelect
-    title="选择钱包"
-    :items="supportedWallets"
+    title="钱包"
+    :items="userAddedWallets"
     @item-click="handleItemClick"
   />
 </template>
 
 <script lang="ts">
-import { supportedWallets } from '@/model/Wallet'
+import { Wallet } from '@/model/Wallet'
 import Group from '@/components/Group.vue'
 import PageSelect from '@/layout/PageSelect.vue'
 import { useRouter } from 'vue-router'
 import { useStore } from '@/store'
+import { onMounted, ref } from 'vue'
+import { readWallets } from '@/db/wallet'
 
 export default {
   components: { Group, PageSelect },
   setup() {
     const store = useStore()
     const router = useRouter()
+    const userAddedWallets = ref<Wallet[]>([])
+
+    onMounted(() => {
+      readWallets().then((wallets) => {
+        userAddedWallets.value = wallets
+      })
+    })
+
     const handleItemClick = (index: number) => {
-      const selectedWallet = supportedWallets[index]
+      const selectedWallet = userAddedWallets.value[index]
       store.commit('setAddRecord', {
         wallet: selectedWallet.name,
       })
@@ -27,7 +37,7 @@ export default {
     }
 
     return {
-      supportedWallets,
+      userAddedWallets,
       handleItemClick,
     }
   },
