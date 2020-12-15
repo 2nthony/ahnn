25ad78b00ea54ee052901f94944300a2dac4ddf9
evillt <ijoec123@gmail.com>
2020-12-15 22:09:26 +0800

    feat: pwa

---
+++ build/pwa.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Ahnn',
+  themeColor: '#000000',
+  msTileColor: '#000000',
+
+  appleMobileWebAppCapable: 'yes',
+  appleMobileWebAppStatusBarStyle: 'black-translucent',
+}
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
   chainWebpack: require('./build/webpack'),
+  pwa: require('./build/pwa'),
 }
