fe83ed8e8fb7f0dfa641ea78607abe76378eefae
evillt <ijoec123@gmail.com>
2020-12-15 23:09:24 +0800

    feat: gtag

--- src/hooks/collect.ts
+++ src/hooks/collect.ts
@@ -1,9 +1,11 @@
 import { initWallet } from './initWallet'
 import { readRecordsFromDB } from './readRecordsFromDB'
 import { restorePreference } from './restorePreference'
+import { setupGtag } from './setupGtag'
 
 export function collect() {
   restorePreference()
   readRecordsFromDB()
   initWallet()
+  setupGtag()
 }
