37fc06f17eb86861eb5271448a9f3ee6804fb001
evillt <ijoec123@gmail.com>
2020-12-28 18:57:43 +0800

    refactor(mixin-events): use watch immediate instead

--- src/mixin-events/refreshRecords.ts
+++ src/mixin-events/refreshRecords.ts
@@ -4,10 +4,14 @@ import { computed, watch } from 'vue'
 export function eventRefreshRecords() {
   const store = useStore()
 
-  store.dispatch('readRecordsByQueryDate')
-
   const recordsQueryDate = computed(() => store.getters.recordsQueryDate)
-  watch(recordsQueryDate, () => {
-    store.dispatch('readRecordsByQueryDate')
-  })
+  watch(
+    recordsQueryDate,
+    () => {
+      store.dispatch('readRecordsByQueryDate')
+    },
+    {
+      immediate: true,
+    },
+  )
 }
