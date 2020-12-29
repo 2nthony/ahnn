f1fa96031f1b77ac867b6af32827ca99d09df2d0
evillt <ijoec123@gmail.com>
2020-12-29 19:48:34 +0800

    feat(tailwind): extend flex to 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      flex: {
+        2: '2 2 0%',
+        3: '3 3 0%',
+      },
+    },
   },
   variants: {
     extend: {},
