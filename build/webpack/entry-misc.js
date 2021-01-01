bf5e7cb9a9c8ae3430054980fb07c0ab71bdae36
evillt <ijoec123@gmail.com>
2021-01-02 02:48:09 +0800

    fix: set prefer appreance

--- build/webpack/entry-misc.js
+++ build/webpack/entry-misc.js
@@ -3,10 +3,6 @@ const { isProd } = require('../shared')
 exports.name = 'entry:misc'
 
 exports.apply = (config) => {
-  // 设置主题偏好
-  // 应该在app挂载之前设置主题防止闪屏
-  config.entryPoints.get('app').prepend('./src/app-entries/prefer-appreance.ts')
-
   const entryMisc = config.entry('misc')
 
   // ios 系统设置 ontouchstart
